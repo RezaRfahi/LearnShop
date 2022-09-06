9358fe6b5c543a8648737ef91f6dfcc75a9b98de
RezaRfahi <refahireza82@gmail.com>
2022-09-07 01:53:27 +0430

    set fillable and sluggable in Article model

--- LearnShop/app/Models/Article.php
+++ LearnShop/app/Models/Article.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Article extends Model
 {
+    use Sluggable;
     use HasFactory;
+
+    protected $fillable =[
+        'title', 'body'
+    ];
+
+    public function sluggable(): array
+    {
+
+        return [
+          'slug' => [
+              'source' => 'title'
+          ]
+        ];
+
+    }
+
 }
