ae7d4621be2531d67ae8511fc39356821e75795d
RezaRfahi <refahireza82@gmail.com>
2022-09-07 16:36:53 +0430

    edit permission_user migration

--- LearnShop/database/migrations/2022_09_07_120213_create_purchases_table.php
+++ LearnShop/database/migrations/2022_09_07_120213_create_purchases_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('purchases', function (Blueprint $table) {
-            $table->id();
+            $table->string('serial_number')->primary();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('course_id')->references('id')->on('courses');
             $table->timestamps();
         });
     }
