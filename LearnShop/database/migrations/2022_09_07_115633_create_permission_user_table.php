c8208d527b1c0a9e226be83d55727f2e1266178f
RezaRfahi <refahireza82@gmail.com>
2022-09-07 16:31:00 +0430

    edit permissiom_uset table

--- LearnShop/database/migrations/2022_09_07_115633_create_permission_user_table.php
+++ LearnShop/database/migrations/2022_09_07_115633_create_permission_user_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('permission_user', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('permission_id');
+            $table->foreign('permission_user')->references('id')->on('permissions');
         });
     }
 
