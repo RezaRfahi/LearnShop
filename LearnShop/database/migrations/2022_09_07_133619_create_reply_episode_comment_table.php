69e5c2252504a3ccfb4ad9cda67d456690cadfc8
RezaRfahi <refahireza82@gmail.com>
2022-09-07 18:37:09 +0430

    edit reply_episode_comment migration

---
+++ LearnShop/database/migrations/2022_09_07_133619_create_reply_episode_comment_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reply_episode_comment', function (Blueprint $table) {
+            $table->unsignedBigInteger('parent_comment_id');
+            $table->unsignedBigInteger('child_comment_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reply_episode_comment');
+    }
+};
